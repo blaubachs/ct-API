2b9e491e559cd8ca623300fabb75f0cb6f0e04c3
Ben L <veloswim@gmail.com>
2023-05-27 12:38:15 -0700

    added userRoutes file

--- controllers/api/index.ts
+++ controllers/api/index.ts
@@ -1,5 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import userRoutes from "./userRoutes"
 
 const router = express.Router();
+
+router.use('/users', userRoutes)
 
 export default router;
