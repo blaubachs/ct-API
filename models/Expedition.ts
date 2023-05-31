2851165dbcd4e7acb1c2d8920efe06e7f76c9bf0
Ben L <veloswim@gmail.com>
2023-05-31 11:32:52 -0700

    updates to expidition and message schema

--- models/Expedition.ts
+++ models/Expedition.ts
@@ -1,5 +1,26 @@
 import { Schema, model } from "mongoose";
 
+const OptionsSchema = new Schema({
+  critFailThresh: {
+    type: Number,
+    max: [50, "Crit fail cannot be higher than 50."],
+    required: true,
+  },
+  failThresh: {
+    type: Number,
+    required: true,
+  },
+  successThresh: {
+    type: Number,
+    required: true,
+  },
+  critSuccessThresh: {
+    type: Number,
+    max: [100, "Cannot set crit success above 100."],
+    required: true,
+  },
+});
+
 const ExpeditionSchema = new Schema({
   name: {
     type: String,
@@ -8,6 +29,7 @@ const ExpeditionSchema = new Schema({
     minlength: [4, "Expedition name must be at least 4 characters long."],
     maxlength: [40, "Expedition name must be less than 40 characters long."],
   },
+  options: { type: OptionsSchema },
   owner: { type: Schema.Types.ObjectId, ref: "User" },
   members: [{ type: Schema.Types.ObjectId, ref: "User" }],
   characters: [{ type: Schema.Types.ObjectId, ref: "Character" }],
--- models/Message.ts
+++ models/Message.ts
@@ -6,6 +6,17 @@ const MessageSchema = new Schema({
     type: String,
     required: true,
   },
+  inCharacter: {
+    type: Boolean,
+    required: true,
+  },
+  character: {
+    type: Schema.Types.ObjectId,
+    ref: "Character",
+    required: function () {
+      return this.inCharacter;
+    },
+  },
 });
 
 const Message = model("Message", MessageSchema);
