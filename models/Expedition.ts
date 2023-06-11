d87fe6cc11887a31214ea0ebb4d75e47c58130d1
Ben L <veloswim@gmail.com>
2023-06-11 13:14:59 -0700

    fixed an issue with expedition schema not creating default options

--- models/Expedition.ts
+++ models/Expedition.ts
@@ -4,19 +4,23 @@ const OptionsSchema = new Schema({
   critFailThresh: {
     type: Number,
     max: [50, "Crit fail cannot be higher than 50."],
+    default: 25,
     required: true,
   },
   failThresh: {
     type: Number,
+    default: 50,
     required: true,
   },
   successThresh: {
     type: Number,
+    default: 75,
     required: true,
   },
   critSuccessThresh: {
     type: Number,
     max: [100, "Cannot set crit success above 100."],
+    default: 90,
     required: true,
   },
 });
@@ -29,7 +33,7 @@ const ExpeditionSchema = new Schema({
     minlength: [4, "Expedition name must be at least 4 characters long."],
     maxlength: [40, "Expedition name must be less than 40 characters long."],
   },
-  options: { type: OptionsSchema },
+  options: { type: OptionsSchema, default: {} },
   owner: { type: Schema.Types.ObjectId, ref: "User" },
   members: [{ type: Schema.Types.ObjectId, ref: "User" }],
   characters: [{ type: Schema.Types.ObjectId, ref: "Character" }],
